693e4103324218f2bcb83e2e0f5c06b2325e1a9c
alamin40 <hossain.78295@gmail.com>
2024-05-12 23:03:10 +0200

    adding conf for post

--- BlogProject/myApp/blog/admin.py
+++ BlogProject/myApp/blog/admin.py
@@ -8,6 +8,15 @@ from .models import Category, Post
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('image_tag','title', 'description', 'url', 'add_date')
   search_fields = ('title',)
+  
+
+# for post confisuration
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('image_tag', 'title', 'url', 'cat' )
+  search_fields = ('title',)
+  list_filter = ('cat',)
+
+
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- BlogProject/myApp/blog/models.py
+++ BlogProject/myApp/blog/models.py
@@ -32,4 +32,5 @@ class Post(models.Model):
   cat = models.ForeignKey(Category, on_delete=models.CASCADE)
   image = models.ImageField(upload_to='post/')
   
-  
+  def image_tag(self):
+    return format_html('<img src="/media/{}" style="width:40px; height:40px; border-radius:40px" />'.format(self.image))
